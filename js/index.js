b434bd9104bb142a29c5946c9ab6fe1094d3036d
Marcelo Cardoso Filho <marcelobuenocardosofilho@gmail.com>
2022-08-18 13:11:41 -0300

    confimar

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector('#show');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function() {
+    
+    if(container.style.display === 'block') {
+       container.style.display = 'none'; 
+    } else {
+        container.style.display = 'block';
+    }
+});
+
+function esconder(){
+    var div = document.getElementById("teste");
+    div.style.display = 'none';
+}
+
+/*======================================================================================*/
+
+
