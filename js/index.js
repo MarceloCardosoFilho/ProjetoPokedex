ec0b71d76d6efc719873fa26bb8344a55c30bf69
Marcelo Cardoso Filho <marcelobuenocardosofilho@gmail.com>
2022-08-22 12:41:47 -0300

    altura, peso e abilities

--- js/index.js
+++ js/index.js
@@ -2,39 +2,40 @@ var formulario = document.querySelector('form')
 
 formulario.addEventListener('submit', function(e){
 
-    e.preventDefault()
+    e.preventDefault();
 
     let urlForm = "https://pokeapi.co/api/v2/pokemon/";
-    let nome = document.getElementById("name")
+    let nome = document.getElementById("name");
 
-    urlForm = urlForm + this.name.value
-    urlForm = urlForm.toLocaleLowerCase()
+    urlForm = urlForm + this.name.value;
+    urlForm = urlForm.toLocaleLowerCase();
 
-    let resposta = document.getElementById('content')
+    let resposta = document.getElementById('content');
 
-    let imagem = document.getElementById('imagemPokemon')
+    let imagem = document.getElementById('imagemPokemon');
 
-    let html = ''
+    let html = '';
+
 
     fetch(urlForm)
         .then(resposta => resposta.json())
         .then(function (data) {
+            var altura = data.height
+
             console.log(data)
-            html = '<b>Nome</b>: ' + maiuscula(data.name) + '<br>'
-            html = html + '<b>Type</b>: ' +maiuscula(data.types[0].type.name)
-            resposta.innerHTML = html
+            html = '<b>Nome</b>: ' + maiuscula(data.name) + '<br>';
+            html = html + '<b>Type</b>: ' +maiuscula(data.types[0].type.name) + '<br>';
+            const  abilities = data.abilities.map((i) => i.ability.name);
+                console.log(abilities)
+                html = html + '<b>Abilites</b>: ' + abilities.join(', ') + '<br>';
+            html = html + '<b>Height</b>: ' +(altura) + ' decimeters <br>';
+            html = html + '<b>Weight</b>: ' +(data.weight) + ' hectogram <br>';
+            resposta.innerHTML = html;
             
             imagem.innerHTML = "<img src='" + data.sprites.front_default + "'> <img src='" + data.sprites.back_default + "'>"
+
+            
         })
-        .catch(function (err){
-            if(err == 'SyntaxError: Unexpected token N in JSON at position 0'){ 
-                html = 'Pokemon não encontrado!'
-            }else{
-                html = err
-            }
-            resposta.innerHTML = html
-        })    
-    
 });
 
 function maiuscula(val){
